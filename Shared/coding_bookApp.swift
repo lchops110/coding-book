b37c3b2a1b8593bbf85395509911f8aa7c4f62c6
lochlan <lochlan110@gmail.com>
2022-07-23 19:26:00 +0100

    Initial Commit

---
+++ Shared/coding_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  coding_bookApp.swift
+//  Shared
+//
+//  Created by Lochlan Thomas on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct coding_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
